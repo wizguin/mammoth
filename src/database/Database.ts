9114d74eac0f4c170dd1a1353fa80b2a9f3d3890
wizguin <16276645+wizguin@users.noreply.github.com>
2023-06-05 14:40:26 +0100

    Added database system base

---
+++ src/database/Database.ts
@@ -0,0 +1,25 @@
+import { PrismaClient } from '@prisma/client'
+
+
+class Database extends PrismaClient {
+
+    constructor() {
+        super()
+
+        this.authenticate()
+    }
+
+    async authenticate() {
+        try {
+            await this.$queryRaw`SELECT 1`
+
+            console.log('Sucessfully connected to database')
+
+        } catch (error) {
+            console.log(`${error}`)
+        }
+    }
+
+}
+
+export default new Database()
