b5c9efd981200227743b4b4c9de969f1d6ea5771
wizguin <16276645+wizguin@users.noreply.github.com>
2023-06-05 14:43:00 +0100

    Added user object

---
+++ src/objects/user/User.ts
@@ -0,0 +1,64 @@
+import Database from '@Database'
+import Delimiter from '../../handler/packet/Delimiter'
+
+import type { User as PrismaUser } from '@prisma/client'
+import type { Socket } from 'net'
+
+
+export default class User implements PrismaUser {
+
+    socket: Socket
+    address: Socket['remoteAddress']
+
+    id!: number
+    username!: string
+    email!: string | null
+    password!: string
+    loginKey!: string | null
+    rank!: boolean
+    permaBan!: boolean
+    joinTime!: Date
+    coins!: number
+    head!: number
+    face!: number
+    neck!: number
+    body!: number
+    hand!: number
+    feet!: number
+    color!: number
+    photo!: number
+    flag!: number
+
+    constructor(socket: Socket) {
+        this.socket = socket
+
+        this.address = socket.remoteAddress
+    }
+
+    sendXml(data: string) {
+        this.write(data)
+    }
+
+    write(data: string) {
+        console.log('sending', data)
+        this.socket.write(`${data}${Delimiter}`)
+    }
+
+    async load(username: string) {
+        const user = await Database.user.findFirst({
+            where: {
+                username: username
+            },
+            include: {
+                inventory: true
+            }
+        })
+
+        if (!user) return false
+
+        Object.assign(this, user)
+
+        return true
+    }
+
+}
