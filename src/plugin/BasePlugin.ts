28a386dd7f6b5d597432eacd5b9dfd9c0cc1f150
wizguin <16276645+wizguin@users.noreply.github.com>
2024-04-25 09:58:27 +0100

    Add ts-runtime-checks types to BasePlugin

--- src/plugin/BasePlugin.ts
+++ src/plugin/BasePlugin.ts
@@ -1,12 +1,19 @@
 import type Handler from '../handler/Handler'
 import type User from '@objects/user/User'
 
+import { Assert } from 'ts-runtime-checks'
+
+export type Num = Assert<number>
+export type NumArray = Assert<number[]>
+export type Str = Assert<string>
+
+type EventHandler = (user: User, ...args: any[]) => void | Promise<void>
 
 export default class BasePlugin {
 
     handler: Handler
     users: User[]
-    events!: Record<string, (args: any, user: User) => void>
+    events!: Record<string, EventHandler>
 
     constructor(handler: Handler) {
         this.handler = handler
