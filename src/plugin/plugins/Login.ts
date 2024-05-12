9bde07e9217154edf905db6f3a64109dd01339af
wizguin <16276645+wizguin@users.noreply.github.com>
2024-05-12 18:01:58 +0100

    Update login and check login key

--- src/plugin/plugins/Login.ts
+++ src/plugin/plugins/Login.ts
@@ -4,6 +4,7 @@ import { allowedVersions } from '@Config'
 import { handleOnce } from '@Decorators'
 import type User from '@objects/user/User'
 
+import { compare } from 'bcrypt'
 import type { Element } from 'elementtree'
 
 export default class Login extends BasePlugin {
@@ -28,26 +29,43 @@ export default class Login extends BasePlugin {
 
     @handleOnce
     async login(user: User, body: Element) {
+        const isAuthenticated = await this.authenticateUser(user, body)
+
+        user.update({ loginKey: null })
+
+        if (!isAuthenticated) {
+            user.disconnect()
+            return
+        }
+
+        this.updateUsersById(user)
+        user.send('l')
+    }
+
+    async authenticateUser(user: User, body: Element) {
         const login = body.find('login')
 
-        if (!login) return
+        if (!login) return false
 
         const nick = login.find('nick')?.text
         const pword = login.find('pword')?.text
 
-        if (!nick || !pword) return
+        if (!nick || !pword) return false
 
-        const load = await user.load(nick.toString())
+        const isLoaded = await user.load(nick.toString())
 
-        if (!load) return
+        if (!isLoaded || !user.loginKey) return false
 
+        // Swap bcrypt prefix
+        return compare(pword.toString(), user.loginKey.replace('$2y$', '$2a$'))
+    }
+
+    updateUsersById(user: User) {
         if (user.id in this.usersById) {
             this.usersById[user.id].disconnect()
         }
 
         this.usersById[user.id] = user
-
-        user.send('l')
     }
 
 }
