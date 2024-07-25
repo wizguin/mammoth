c3585e10782d0f807a9faf66b492fe0fcc55944b
wizguin <16276645+wizguin@users.noreply.github.com>
2024-07-25 18:22:23 +0100

    Add additional data loading

--- src/data/Data.ts
+++ src/data/Data.ts
@@ -2,13 +2,26 @@ import { loadData } from './LoadData'
 
 import type { Assert } from 'ts-runtime-checks'
 
+type Furnitures = Record<string, Furniture>
 type Items = Record<string, Item>
+type PlayerRooms = Record<string, PlayerRoom>
+type Rooms = Room[]
+
+interface Furniture {
+    name: string,
+    cost: number
+}
 
 interface Item {
     name: string,
     type: number,
     cost: number,
     member: boolean
+}
+
+interface PlayerRoom {
+    name: string,
+    cost: number
 }
 
 interface Room {
@@ -20,5 +33,7 @@ interface Room {
     spawn: boolean
 }
 
+export const furnitures: Furnitures = loadData('furnitures') as Assert<Furnitures>
 export const items: Items = loadData('items') as Assert<Items>
-export const rooms: Room[] = loadData('rooms') as Assert<Room[]>
+export const playerRooms: PlayerRooms = loadData('player_rooms') as Assert<PlayerRooms>
+export const rooms: Rooms = loadData('rooms') as Assert<Rooms>
