b6486837df4818acf0e81c6ff1b997bf1b2f9475
wizguin <16276645+wizguin@users.noreply.github.com>
2024-07-26 13:24:19 +0100

    Add ball plugin

---
+++ src/plugin/plugins/Ball.ts
@@ -0,0 +1,30 @@
+import BasePlugin, { type Num } from '../BasePlugin'
+
+import type User from '@objects/user/User'
+
+export default class Ball extends BasePlugin {
+
+    events = {
+        gz: this.getGame,
+        zm: this.sendMove
+    }
+
+    ballX = 397
+    ballY = 240
+
+    getGame(user: User) {
+        if (user.room?.name !== 'Rink') return
+
+        user.send('gz', this.ballX, this.ballY)
+    }
+
+    sendMove(user: User, x: Num, y: Num) {
+        if (user.room?.name !== 'Rink') return
+
+        this.ballX = x
+        this.ballY = y
+
+        user.sendRoom('zm', x, y)
+    }
+
+}
