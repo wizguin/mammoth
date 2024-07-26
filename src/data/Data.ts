1a98ac09019b8f6634b656f4e86460c9ae6d4b31
wizguin <16276645+wizguin@users.noreply.github.com>
2024-07-26 16:52:52 +0100

    Add load waddle data

--- src/data/Data.ts
+++ src/data/Data.ts
@@ -6,6 +6,7 @@ type Furniture = Record<string, FurnitureItem>
 type Items = Record<string, Item>
 type PlayerRooms = Record<string, PlayerRoom>
 type Rooms = Room[]
+type Waddles = Waddle[]
 
 interface FurnitureItem {
     name: string,
@@ -33,7 +34,15 @@ interface Room {
     spawn?: boolean
 }
 
+interface Waddle {
+    id: number,
+    roomId: number,
+    seats: number,
+    game: string
+}
+
 export const furniture: Furniture = loadData('furniture') as Assert<Furniture>
 export const items: Items = loadData('items') as Assert<Items>
 export const playerRooms: PlayerRooms = loadData('player_rooms') as Assert<PlayerRooms>
 export const rooms: Rooms = loadData('rooms') as Assert<Rooms>
+export const waddles: Waddles = loadData('waddles') as Assert<Waddles>
