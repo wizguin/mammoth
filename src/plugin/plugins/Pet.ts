648f3cb7b18264b34f76017387c908b1b278eabc
wizguin <16276645+wizguin@users.noreply.github.com>
2024-08-03 12:36:16 +0100

    Add pet plugin

---
+++ src/plugin/plugins/Pet.ts
@@ -0,0 +1,60 @@
+import BasePlugin, { type Num, type Str } from '../BasePlugin'
+
+import type User from '@objects/user/User'
+
+export default class Pet extends BasePlugin {
+
+    events = {
+        cw: this.checkWord,
+        n: this.namePet,
+        a: this.addPet,
+        g: this.getPets,
+        r: this.sendRest,
+        p: this.sendPlay,
+        f: this.sendFeed,
+        t: this.sendTreat,
+        s: this.sendPetFrame,
+        m: this.sendMovePet
+    }
+
+    checkWord(user: User, word: Str) {
+        user.send('cw', word, 1)
+    }
+
+    namePet(user: User, typeId: Num, name: Str) {
+
+    }
+
+    addPet(user: User) {
+
+    }
+
+    getPets(user: User, userId: number) {
+        user.send('p', 'g', user.pets)
+    }
+
+    sendRest(user: User) {
+
+    }
+
+    sendPlay(user: User) {
+
+    }
+
+    sendFeed(user: User) {
+
+    }
+
+    sendTreat(user: User) {
+
+    }
+
+    sendPetFrame(user: User) {
+
+    }
+
+    sendMovePet(user: User, petId: Num, x: Num, y: Num) {
+        user.room?.send('p', 'm', petId, x, y)
+    }
+
+}
