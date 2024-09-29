7652c8658b29fb9a0515e704ed4a1f308c48c56e
wizguin <16276645+wizguin@users.noreply.github.com>
2024-09-29 18:57:50 +0100

    Add concurrently dev script

---
+++ scripts/dev.js
@@ -0,0 +1,23 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const concurrently = require('concurrently')
+const path = require('path')
+
+const command = 'npm run dev'
+
+const { result } = concurrently(
+    [
+        { command, name: 'api', cwd: path.resolve('packages/api') },
+        { command, name: 'join', cwd: path.resolve('packages/join') },
+        { command, name: 'policy', cwd: path.resolve('packages/policy') },
+        { command, name: 'world', cwd: path.resolve('packages/world') }
+    ],
+    {
+        prefix: '{name}\t|',
+        prefixColors: 'auto'
+    }
+)
+
+result.then(
+    () => {},
+    error => console.error(error)
+)
